5acd78cb59a6fa4dbf61a1080d2731ef4da89a1d
Khoby-790 <ebaidoo79@gmail.com>
2021-02-22 13:10:09 +0000

    --- Auto-Git Commit ---

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'khoby-search-select'
+import  ExampleComponent  from 'khoby-search-select'
 import 'khoby-search-select/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent options={[]} />
 }
 
 export default App
