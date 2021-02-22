d2717ff6d0c367f8095e12b96aa9ae06c825cdb9
Khoby-790 <ebaidoo79@gmail.com>
2021-02-22 12:59:29 +0000

    --- Auto-Git Commit ---

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,42 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+// import styles from './styles.module.css'
+// import uuid from 'uuid';
 
 interface Props {
-  text: string
+  label?: string
+  options: any[]
+  itemClassName?: string
+  containerClassName?: string
+  searchBoxClassName?: string
+  component?: React.ComponentType<SelectItem>
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export type SelectItem = {
+  label: string
+  value: any
 }
+
+const SearchSelect = ({ component: Component, ...props }: Props) => {
+  return (
+    <div className={props.containerClassName}>
+      <label htmlFor={`${new Date().getMilliseconds()}`}>{props.label}</label>
+      <div>
+        <input type='text' />
+      </div>
+      {Component &&
+        props.options.map((item: SelectItem, itemIndex) => (
+          <Component key={itemIndex} {...item} />
+        ))}
+      <ul>
+        {!Component &&
+          props.options.map((item: SelectItem, itemIndex) => (
+            <li key={itemIndex} className={props?.itemClassName}>
+              {item.label}
+            </li>
+          ))}
+      </ul>
+    </div>
+  )
+}
+
+export default SearchSelect
